012f4ca6f4adfa7222976cb58ff2de148d83af8f
Gabor Orosz <goro@goro.hu>
2014-07-13 13:50:32 +0200

    Setting up GTest and GMock framework

--- main.cpp
+++ main.cpp
@@ -1,10 +1,7 @@
-#include <iostream>
+#include <gmock/gmock.h>
 
-using namespace std;
-
-int main()
+int main(int argc, char** argv)
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    testing::InitGoogleMock(&argc, argv);
+    return RUN_ALL_TESTS();
 }
-
